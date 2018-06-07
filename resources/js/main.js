f1bcbd5335acac3be9196e668451623a1f298360
Rummel17 <p.rummel17@gmail.com>
2018-06-07 22:25:49 +0200

    1.0

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,15 +1,36 @@
-// Initialize and add the map
-function initMap() {
-  // The location of Uluru
-  var ingbert = {lat: 49.285685, lng: 7.150964};
-  // The map, centered at St.Ingbert
-  var map = new google.maps.Map(
-      document.getElementById('map'), {
-        zoom: 12,
-        center: ingbert,
-        zoomControl: true,
-        mapTypeControl: true,
+
+      function initMap() {
+        // The location of Uluru
+        var ingbert = {lat: 49.285647, lng: 7.150917};
+        // The map, centered at St.Ingbert
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: ingbert,
+          zoom: 15,
+        });
+
+        var infowindow = new google.maps.InfoWindow();
+        var service = new google.maps.places.PlacesService(map);
+
+
+        service.getDetails({
+          //place Id to identify the address, look up added to book mark
+          placeId: 'ChIJ9SckLMl9C0ERz6Ja2-mKd44'
+        }, function(place, status) {
+          if (status === google.maps.places.PlacesServiceStatus.OK) {
+            // The marker, positioned at St.Ingbert
+      
+            var marker = new google.maps.Marker({
+              map: map,
+              position: ingbert,
+
             });
-  // The marker, positioned at St.Ingbert
-  var marker = new google.maps.Marker({position: ingbert, map: map});
-};
+            google.maps.event.addListener(marker, 'click', function() {
+              infowindow.setContent('<div class="address"><strong>' + place.name + //'</strong><br>' +
+                //'Place ID: ' + place.place_id +
+                '<br>' +
+                place.formatted_address + '</div>');
+              infowindow.open(map, this);
+            });
+          }
+        });
+      }
