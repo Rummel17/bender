f348f8b1372470bef825e00151f823de4a43ad2f
Rummel17 <p.rummel17@gmail.com>
2018-09-09 10:08:12 +0200

    final 1.0

--- resources/js/shrink.js
+++ resources/js/shrink.js
@@ -3,12 +3,14 @@ $(document).on("scroll", function(){
     ($(document).scrollTop() > 100){
     $("header").addClass("shrink");
     $(".logo").addClass("logo__small");
+    $(".phonenumber-fontsize").addClass("phonenumber__small");
     $(".cars-image").addClass("cars-image__small");
   }
   else
   {
     $("header").removeClass("shrink");
     $(".logo").removeClass("logo__small");
+    $(".phonenumber-fontsize").removeClass("phonenumber__small");
     $(".cars-image").removeClass("cars-image__small");
   }
 });
